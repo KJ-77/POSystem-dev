95b25b2a2e77e0714adfbde77b2e08242c8c66d6
rhb16 <rhb16@mail.aub.edu>
2024-08-22 08:48:57 +0300

    'initialize'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <>
+      <Typography variant ='h2' color ="primary">
+        WELCOME 
+      </Typography>
+    </>
+  )
+}
+
+export default App
