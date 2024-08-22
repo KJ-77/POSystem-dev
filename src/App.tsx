ff6bfb74cf23a4d093c1229a41a7afbbc6c2c9a1
khalilelset <khoulioelset2000@gmail.com>
2024-08-22 10:15:29 +0300

    install some lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './App.css'
-import { Typography } from '@mui/material'
+import "./App.css";
+//import { Typography } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <Typography variant ='h2' color ="primary">
-        WELCOME 
-      </Typography>
+      <Routes>
+        <Route path="/" element={<></>} />
+        {/*user_id !== null ? <Route path="Todo" element={<></>} /> : <></>*/}
+        <Route path="/*" element={<></>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
